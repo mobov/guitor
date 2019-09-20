e388d0025018b362f0cbec04c4a6c489cf295745
nocoolyoyo <574857444@qq.com>
2019-09-20 11:25:07 +0800

    添加HTML预览，修复排序BUG，组件库 模板库分离

--- packages/creator/config/prod.env.js
+++ packages/creator/config/prod.env.js
@@ -3,14 +3,13 @@
  * Created by nocoolyoyo on 2018/2/18.
  */
 
-
 const formatEnv = require('./_formatEnv')
 
 const env = {
   // 环境变量名
   NODE_ENV: 'production',
   // 提供导出包CDN的默认地址
-  NPM_CDN: 'http://unpkg.zhimg.com/'
+  NPM_CDN: 'http://unpkg.com/'
 }
 
 module.exports = formatEnv(env)
